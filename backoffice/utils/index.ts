7618cc223064ca8c8893000a01f098dea033602c
t1m0t <timothee.baron@pm.me>
2025-12-13 20:12:23 +0100

    formatting

--- backoffice/utils/index.ts
+++ backoffice/utils/index.ts
@@ -1,10 +1,13 @@
-export function checkAllKeys(obj: any, callback: (key: string, value: any) => void) {
-  for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      callback(key, obj[key]);
-      if (typeof obj[key] === 'object' && obj[key] !== null) {
-        checkAllKeys(obj[key], callback);
-      }
-    }
-  }
+export function checkAllKeys(
+	obj: any,
+	callback: (key: string, value: any) => void,
+) {
+	for (const key in obj) {
+		if (Object.prototype.hasOwnProperty.call(obj, key)) {
+			callback(key, obj[key]);
+			if (typeof obj[key] === "object" && obj[key] !== null) {
+				checkAllKeys(obj[key], callback);
+			}
+		}
+	}
 }

a304a706228892c415d0314a2e44076df0f39212
t1m0t <timothee.baron@pm.me>
2025-12-14 08:27:26 +0100

    fix runtime and don't include db in build and dev

--- backoffice/src/tile/tile.module.ts
+++ backoffice/src/tile/tile.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { ScheduleModule } from "@nestjs/schedule";
 import { TileController } from "./tile.controller";
-import { PostgresCacheService } from "./postgres-cache.service";
 import { HttpModule } from "@nestjs/axios";
 
 @Module({
@@ -10,7 +9,5 @@ import { HttpModule } from "@nestjs/axios";
 		HttpModule,
 	],
 	controllers: [TileController],
-	providers: [PostgresCacheService],
-	exports: [PostgresCacheService],
 })
 export class TileModule {}
